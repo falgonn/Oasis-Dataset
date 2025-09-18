3d67f6f6fc38d1b5dd123a990e47cfe4ea4304f4
kavya <82271852+falgonn@users.noreply.github.com>
2025-09-18 22:38:04 +1000

    Update CIFARCODE.py

--- CIFAR-10/CIFARCODE.py
+++ CIFAR-10/CIFARCODE.py
@@ -111,7 +111,6 @@ class ResNet18(nn.Module):
         super(ResNet18, self).__init__()
         self.in_planes = 64
 
-        # Your original architecture was correct
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
 
